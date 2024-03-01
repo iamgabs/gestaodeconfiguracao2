6b15499b0655dfbe50b0c0838eb85ef6483f1469
iamgabs <anjomito26@gmail.com>
2024-02-29 21:25:26 -0300

    chore: create post route

--- src/app.py
+++ src/app.py
@@ -18,6 +18,15 @@ def get_products():
     return jsonify(products)
 
 
+@app.route("/new_product", methods=["post"])
+def create_product():
+    """
+    create new product
+    """
+    new_product = request.json
+    products.append(new_product)
+    return jsonify(new_product), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
