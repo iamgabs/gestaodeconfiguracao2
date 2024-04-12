1b757f6096569096a49d7e54b0333d680e86a948
Gabriel Carvalho <80786139+iamgabs@users.noreply.github.com>
2024-04-11 21:46:36 -0300

    fix

--- tests/app_test.py
+++ tests/app_test.py
@@ -6,7 +6,7 @@ from typing import Generator
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
-def client() -> typing.Generator:
+def client() -> Generator:
     with app.test_client() as client:
         yield client
 
